b556f14438f5f342ee65b0b0081784196d727711
dantran <namdan.tran3003@gmail.com>
2023-11-09 10:25:12 +0700

    add reservation

--- controllers/reservation.controller.js
+++ controllers/reservation.controller.js
@@ -6,8 +6,8 @@ const create_reservation = async (req, res) => {
         console.log(req.body)
         const body = req.body;
         const reservationJson = {
-            user_id: body.user_id,
-            hotel_id: body.hotel_id,
+            user_id: body.user_id.toString(),
+            hotel_id: body.hotel_id.toString(),
             is_cancelled: false,
             rooms: body.rooms,
             create_date: new Date(),
@@ -27,7 +27,7 @@ const get_history = async (req, res) => {
     // need user_id
     try {
         const id = req.params.id;
-        const snapshot = await db.collection("reservations").where("user_id", "==", parseInt(id)).get();
+        const snapshot = await db.collection("reservations").where("user_id", "==", id.toString()).get();
         let responseArr = [];
         snapshot.forEach(doc => {
             responseArr.push(doc.data());
@@ -39,5 +39,57 @@ const get_history = async (req, res) => {
     }
 }
 
+const cancel_reservation = async (req, res) => {
+    // need reservation_id
+    try {
+        const id = req.params.id;
+        const document = db.collection("reservations").doc(id);
+        const response = await document.update({is_cancelled: true});
+        res.send(response);
+    } catch (error) {
+        res.send(error);
+    }
+}
 
-module.exports = {create_reservation, get_history}
+const create_feedback = async (req, res) => {
+    // need reservation_id, user_id, hotel_id, feedback
+    try {
+        console.log(req.body);
+        const body = req.body;
+        const feedbackJson = {
+            stars: body.stars,
+            feedback: body.feedback,
+        };
+        
+        // todos: recalculate hotel's rating
+        
+        const response = db.collection("reservations").doc(body.reservation_id).set(feedbackJson, {merge: true})
+        res.send(response);
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+const get_feedback = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const document = db.collection("reservations").where("hotel_id", "==", id);
+        const snapshot = await document.get();
+        let responseArr = [];
+        snapshot.forEach(doc => {
+            if (doc.data().feedback)
+                responseArr.push(doc.data());
+        });
+        res.send(responseArr);
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+module.exports = {
+    create_reservation,
+    get_history,
+    cancel_reservation,
+    create_feedback,
+    get_feedback
+}
--- routes/reservation.route.js
+++ routes/reservation.route.js
@@ -7,4 +7,13 @@ router.route('/')
 router.route('/history/:id')
     .get(reservation_controller.get_history)
 
+router.route('/cancel/:id')
+    .put(reservation_controller.cancel_reservation)
+
+router.route('/createFeedback')
+    .post(reservation_controller.create_feedback)
+
+router.route('/getFeedback/:id')
+    .get(reservation_controller.get_feedback)
+
 module.exports = router
